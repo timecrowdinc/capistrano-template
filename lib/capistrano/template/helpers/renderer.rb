eceb6ce25f8560ca8eafdf9e9c6a89cb5b9fb849
tnantoka <tnantoka@bornneet.com>
2025-11-27 13:58:25 +0900

    feat: change ERB inialization for ERB v6

--- lib/capistrano/template/helpers/renderer.rb
+++ lib/capistrano/template/helpers/renderer.rb
@@ -19,7 +19,7 @@ module Capistrano
         end
 
         def as_str
-          @rendered_template ||= ERB.new(template_content, nil, '-').result(Kernel.binding)
+          @rendered_template ||= ERB.new(template_content, trim_mode: '-').result(Kernel.binding)
         end
 
         def as_io
